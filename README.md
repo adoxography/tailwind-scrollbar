d4f10721f9f0346ee036a68f62d14f8cc571d711
Graham Still <gstill@uw.edu>
2020-09-01 17:23:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Scrollbar Plugin for Tailwind CSS
+
+Adds styling utilities for scrollbars in Firefox and webkit-based browsers.
+
+## Installation
+
+```
+yarn add -D tailwind-scrollbar
+```
+or
+```
+npm install --save-dev tailwind-scrollbar
+```
+
+## Usage
+
+Add it to the plugins array of your Tailwind config.
+
+```
+plugins: [
+    // ...
+    require('tailwind-scrollbar')(),
+],
+```
+
+For every element that you want to style, add either the `.scrollbar` or `.scrollbar-thin` class. You may then add any `scrollbar-track-{color}`, `scrollbar-thumb-{color}` or `hover:scrollbar-thumb-{color}` classes you like. (Note that `hover:scrollbar-thumb-{color}` classes only have effects in webkit-based browsers.
+
+## License
+
+This project is licensed under the [MIT License](/LICENSE).
---
+++ index.js
@@ -0,0 +1,87 @@
+const plugin = require('tailwindcss/plugin');
+
+/**
+ * Generates a track style, a thumb style, and a thumb hover style for a given
+ * name/color pair
+ *
+ * @param name  The text to use in the class name
+ * @param color The color to set the element to
+ *
+ * @return An object containing the generated classes
+ */
+const generateScrollbarClasses = (key, value) => ({
+  [`.scrollbar-track-${key}`]: {
+    '--scrollbar-track': value
+  },
+
+  [`.scrollbar-thumb-${key}`]: {
+    '--scrollbar-thumb': value
+  },
+
+  [`.hover\\:scrollbar-thumb-${key}`]: {
+    '&::-webkit-scrollbar-thumb:hover': {
+      '--scrollbar-thumb': value
+    }
+  }
+});
+
+/**
+ * Tells an element what to do with --scrollbar-track and --scrollbar-thumb
+ * variables
+ */
+const scrollbarBase = {
+  '--scrollbar-track': 'initial',
+  '--scrollbar-thumb': 'initial',
+  'scrollbar-color': 'var(--scrollbar-thumb) var(--scrollbar-track)',
+
+  '&::-webkit-scrollbar-track': {
+    'background-color': 'var(--scrollbar-track)'
+  },
+
+  '&::-webkit-scrollbar-thumb': {
+    'background-color': 'var(--scrollbar-thumb)'
+  }
+};
+
+module.exports = plugin(function ({ e, addUtilities, theme, addBase }) {
+  const generateScrollbarColorUtilities = (colors, prefix = '') => Object.entries(colors)
+    .reduce((memo, [key, value]) => ({
+      ...memo,
+      ...(
+        typeof value === 'object'
+        ? generateScrollbarColorUtilities(value, `${e(key)}-`)
+        : generateScrollbarClasses(`${prefix}${e(key)}`, value)
+      )
+    }), {});
+
+  addBase({
+    '*': {
+      'scrollbar-color': 'initial',
+      'scrollbar-width': 'initial'
+    }
+  });
+
+  addUtilities({
+    '.scrollbar': {
+      ...scrollbarBase,
+      'scrollbar-width': 'auto',
+
+      '&::-webkit-scrollbar': {
+        width: '16px',
+        height: '16px'
+      }
+    },
+
+    '.scrollbar-thin': {
+      ...scrollbarBase,
+      'scrollbar-width': 'thin',
+
+      '&::-webkit-scrollbar': {
+        width: '8px',
+        height: '8px'
+      }
+    },
+
+    ...generateScrollbarColorUtilities(theme('colors', {}))
+  });
+});
