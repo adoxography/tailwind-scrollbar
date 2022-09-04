7a89b80c7b9c9d895020ca35dbf2046195e586a2
Graham Still <graham@gstill.dev>
2022-09-04 18:49:58 -0500

    Support arbitrary radii

--- src/utilities.d.ts
+++ src/utilities.d.ts
@@ -73,31 +73,3 @@ export const SCROLLBAR_SIZE_UTILITIES: {
         };
     };
 };
-/**
- * Collapses a nested object into a flat object of suffix/value pairs.
- *
- * @param {object} configObj The object to collapse
- * @param {Function} e       A function that escapes special characters from keys
- * @param {string} sep       The separator between key segments
- * @returns {object} A flat object that maps suffixes to their values
- */
-export function buildSuffixMap(configObj: object, e: Function, sep?: string): object;
-/**
- * Generates a rounded style for a given name/value pair
- *
- * @param {string} key   The text to use in the class name
- * @param {string} value The CSS value to use as the border-radius
- * @returns {object} The generated rounded track and thumb utilities
- */
-export function generateRadiusUtilities(key: string, value: string): object;
-/**
- * Builds a CSS-in-JS object by passing a map of suffixes and values into a
- * function.
- *
- * @param {object} suffixMap A map of string suffixes to CSS values
- * @param {Function} func    A function that consumes class suffixes and CSS
- *                           values to produce a CSS-in-JS object
- * @returns {object} A CSS-in-JS object consisting of all of the objects
- *                   generated for each suffix/value pair
- */
-export function generateUtilitiesFromSuffixes(suffixMap: object, func: Function): object;
