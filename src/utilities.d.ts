c8d19fb717b06d760e156ff65edd79733cfda76f
Graham Still <graham@gstill.dev>
2022-09-04 22:46:38 -0500

    Add husky

--- src/utilities.d.ts
+++ src/utilities.d.ts
@@ -7,101 +7,14 @@ export const BASE_STYLES: {
         'scrollbar-width': string;
     };
 };
+export const COMPONENTS: string[];
 /**
  * Utilities for initializing a custom styled scrollbar, which implicitly set
  * the scrollbar's size
  */
 export const SCROLLBAR_SIZE_UTILITIES: {
-    '.scrollbar': {
-        'scrollbar-width': string;
-        '&::-webkit-scrollbar': {
-            width: string;
-            height: string;
-        };
-        '--scrollbar-track': string;
-        '--scrollbar-thumb': string;
-        '--scrollbar-corner': string;
-        'scrollbar-color': string;
-        overflow: string;
-        '&.overflow-x-hidden': {
-            'overflow-x': string;
-        };
-        '&.overflow-y-hidden': {
-            'overflow-y': string;
-        };
-        '&::-webkit-scrollbar-track': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-thumb': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-corner': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-track:hover': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-thumb:hover': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-corner:hover': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-track:active': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-thumb:active': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-corner:active': {
-            'background-color': string;
-        };
-    };
-    '.scrollbar-thin': {
-        'scrollbar-width': string;
-        '&::-webkit-scrollbar': {
-            width: string;
-            height: string;
-        };
-        '--scrollbar-track': string;
-        '--scrollbar-thumb': string;
-        '--scrollbar-corner': string;
-        'scrollbar-color': string;
-        overflow: string;
-        '&.overflow-x-hidden': {
-            'overflow-x': string;
-        };
-        '&.overflow-y-hidden': {
-            'overflow-y': string;
-        };
-        '&::-webkit-scrollbar-track': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-thumb': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-corner': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-track:hover': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-thumb:hover': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-corner:hover': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-track:active': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-thumb:active': {
-            'background-color': string;
-        };
-        '&::-webkit-scrollbar-corner:active': {
-            'background-color': string;
-        };
-    };
+    '.scrollbar': any;
+    '.scrollbar-thin': any;
     '.scrollbar-none': {
         'scrollbar-width': string;
         '&::-webkit-scrollbar': {
